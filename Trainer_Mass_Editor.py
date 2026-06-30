0bbc82a93089f229de3446f961c4dba16bd50fd8
ABZB <abzb13@gmail.com>
2026-06-29 22:38:42 -0400

    initial commit

---
+++ Trainer_Mass_Editor.py
@@ -0,0 +1,214 @@
+from encodings import utf_16_le, utf_8
+from generic_garc_handling import *
+
+import os
+
+class trainerdata:
+    def __init__(self):
+        game = ''
+        trainer_name_list_royale = ['trainer_list_royale.csv']
+        trainer_name_list_tree = ['trainer_list_tree.csv']
+        trainer_name_list_world = ['trainer_list_world.csv']
+        ability_name_list = ['custom_ability_list.csv']
+        item_name_list = ['custom_item_list.csv']
+        pokemon_name_list = ['custom_pokemon_list.csv']
+        move_name_list = ['custom_move_list.csv']
+
+        trainer_path = ''
+        pokemon_path = ''
+
+        trainer_binary = []
+        pokemon_binary = []
+        personal_binary = []
+
+        trainer_file_name = ''
+        pokemon_file_name = ''
+
+nature_names = ['Hardy', 'Lonely', 'Brave', 'Adamant', 'Naughty', 'Bold', 'Docile', 'Relaxed', 'Impish', 'Lax', 'Timid', 'Hasty', 'Serious', 'Jolly', 'Naive', 'Modest', 'Mild', 'Quiet', 'Bashful', 'Rash', 'Calm', 'Gentle', 'Sassy', 'Careful', 'Quirky']
+
+class_names = ['Pokemon Trainer', 'Pokemon Trainer', 'Youngster', 'Lass', 'Ace Trainer', 'Ace Trainer', 'Preschooler', 'Preschooler', 'Beauty', 'Swimmer', 'Black Belt', 'Scientist', 'Punk Guy', 'Backpacker', 'Punk Girl', 'Sightseer', 'Sightseer', 'Swimmer', 'Veteran', 'Veteran', 'Rising Star', 'Rising Star', 'Madame', 'Gentleman', 'Hiker', 'Collector', 'Pokemon Breeder', 'Pokemon Breeder', 'Team Skull', 'Team Skull', 'Pokemon Trainer', 'Island Kahuna', 'Office Worker', 'Youth Athlete', 'Youth Athlete', 'Swimmer', 'Office Worker', 'Teacher', 'Captain', 'Trial Guide', 'Pokemon Trainer', 'Pokemon Trainer', 'Trial Guide', 'Captain', 'Captain', 'Captain', 'Captain', 'Captain', 'Captain', 'Island Kahuna', 'Island Kahuna', 'Island Kahuna', 'Police Officer', 'Dancer', 'Cook', 'Bellhop', 'Firefighter', 'Janitor', 'Worker', 'Fisherman', 'Golfer', 'Golfer', 'Preschooler', 'Rising Star', 'Ace Trainer', 'Veteran', 'Sightseer', 'Swimmer', 'Punk Guy', 'Team Skull', 'Team Skull', 'Aether President', 'Aether Branch Chief', 'Aether Foundation', 'Aether Foundation', 'Aether Foundation', 'Team Skull Boss', 'Pokemon Trainer', 'Team Skull Admin', 'Pokemon Trainer', 'Elite Four', 'Pokemon Trainer', 'Aether President', 'Pokemon Trainer', 'Pokemon Trainer', 'Pokemon Trainer', 'Pokemon Trainer', 'Pokemon Trainer', 'Pokemon Trainer', 'Pokemon Trainer', 'Pokemon Trainer', 'Pokemon Trainer', 'Pro Wrestler', 'Pokemon Trainer', 'Pokemon Center Lady', 'Aether Foundation', 'Aether Foundation', 'Pokemon Trainer', 'Pokemon Trainer', 'Pokemon Trainer', 'Pokemon Trainer', 'Pokemon Trainer', 'Pokemon Trainer', 'Pokemon Trainer', 'Pokemon Trainer', 'Pokemon Trainer', 'Pokemon Trainer', 'Elite Four', 'Pokemon Trainer', 'Elite Four', 'Elite Four', 'Pokemon Trainer', 'Rising Star', 'Ace Trainer', 'Veteran', 'Sightseer', 'Swimmer', 'Preschooler', 'Black Belt', 'Ace Trainer', 'Ace Trainer', 'Veteran', 'Veteran', 'Black Belt', 'Collector', 'Trial Guide', 'Trial Guide', 'Youngster', 'Pokemon Trainer', 'Principal', 'Flareon User', 'Espeon User', 'Leafeon User', 'Sylveon User', 'Eevee User', 'Umbreon User', 'Vaporeon User', 'Jolteon User', 'Glaceon User', 'GAME FREAK', 'Pokemon Trainer', 'Island Kahuna', 'Captain', 'Pokemon Trainer', 'Punk Girl', 'Youth Athlete', 'Youth Athlete', 'Swimmer', 'Swimmer', 'Golfer', 'Pokemon Trainer', 'Captain', 'Captain', 'Captain', 'Pokemon Professor', 'Ace Trainer', 'Ace Trainer', 'Rising Star', 'Rising Star', 'Youngster', 'Lass', 'Pokemon Breeder', 'Aether Foundation', 'Youngster', 'Island Kahuna', 'Pokemon Professor', 'Pokemon Trainer', 'Pokemon Trainer', 'Pokemon Trainer', 'Pokemon Trainer', 'Pokemon Trainer', 'Pokemon Trainer', 'Twins', 'Rising Star Duo', 'Punk Pair', 'Karate Family', 'Swimmers', 'Ace Duo', 'Honeymooners', 'Veteran Duo', 'Athletic Siblings', 'Swimmer Girls', 'Golf Buddies', 'Battle Legend', 'Battle Legend', 'Aether Foundation', 'Pokemon Trainer', 'Pokemon Trainer', 'Pokemon Trainer', 'Pokemon Trainer', 'Pokemon Trainer', 'Elite Four', 'Ultra Recon Squad', 'Ultra Recon Squad', 'Pokemon Trainer', 'Surfer', 'Actor', 'Reporter', 'Team Aqua', 'Team Galactic', 'Team Magma', 'Team Plasma', 'Team Flare', 'Kantonian Gym', 'Kantonian Gym', 'GAME FREAK', 'Team Rainbow Rocket', 'Pokemon Trainer', 'Team Rainbow Rocket', 'Team Rainbow Rocket', 'Ultra Forest', 'Master & Apprentice', 'Dancing Family', 'Capoeira Couple', 'Tourist Couple', 'Sparring Partners', 'Office Worker', 'Dancer', 'Bellhop', 'Pokemon Trainer', 'Aether President', 'Pokemon Trainer', 'Pokemon Trainer']
+
+def print_regular_trainer_csv(working_data):
+    pass
+
+def print_facility_trainer_csv(working_data):
+    temp = []
+
+    #write Pokemon
+    with open(asksaveasfilename(title='Save Table of Facility Pokemon', defaultextension='.csv',filetypes= [('CSV','.csv')]), 'w', newline = '', encoding='utf-8-sig') as csvfile:
+        writer_head = csv.writer(csvfile, dialect='excel', delimiter=',')
+
+        #write header row
+        writer_head.writerow(['Index', 'Pokemon Name', 'Forme', 'Move 1', 'Move 2', 'Move 3', 'Move 4', 'Perfect IVs', 'Nature', 'Item'])
+
+        for count, entry in enumerate(working_data.pokemon_binary):
+            
+            #pokemon index:
+            pokemon_index = working_data.pokemon_name_list[from_little_bytes_int(entry[0:2])]
+
+            #forme #
+            forme_number = entry[0xE]
+            #if forme is 0, regular base pokemon
+            #otherwise need to get absolute number to get right name
+            if(forme_number != 0):
+                #index of first alt forme + forme number - 1 = index of this particular alt forme (e.g. forme 1 is the pointer index)
+                forme_number = forme_number + from_little_bytes_int(working_data.personal_binary[pokemon_index][0x1C:0x1E]) - 1
+
+            ivs = ''
+            iv_byte = entry[0xA]
+            iv_names = ['HP', 'Atk', 'Def', 'Spe', 'SpA', 'SpD']
+            for x in range(6):
+                if (iv_byte ^ (1 << x) == (1 << x)):
+                    ivs += (' | ' if len(ivs) > 0 else '') + iv_names[x]
+
+            #write row
+            writer_head.writerow([
+                count, 
+                working_data.pokemon_name_list[pokemon_index], 
+                '' if forme_number == 0 else working_data.pokemon_name_list[forme_number], 
+                *(working_data.move_name_list[from_little_bytes_int(entry[2 + x: 4 + x])] for x in range(4)), 
+                ivs, 
+                working_data.item_name_list[from_little_bytes_int(entry[0xC:0xE])]])
+
+            temp.append(str(working_data.pokemon_name_list[pokemon_index]) + '' if forme_number == 0 else working_data.pokemon_name_list[forme_number])
+
+    #write trainers
+    with open(asksaveasfilename(title='Save Table of Facility Trainers', defaultextension='.csv',filetypes= [('CSV','.csv')]), 'w', newline = '', encoding='utf-8-sig') as csvfile:
+        writer_head = csv.writer(csvfile, dialect='excel', delimiter=',')
+        trainer_output = []
+        max_poke_count = 0
+
+        for count, entry in enumerate(working_data.trainer_binary):
+            trainer_temp = []
+            trainer_temp.append(count)
+            #append trainer name
+            trainer_output.append(tree_names[count])
+            #append trainer class
+            trainer_output.append[class_names[from_little_bytes_int(entry[0x0:0x2])]]
+            #append mystery value
+            trainer_output.append[class_names[from_little_bytes_int(entry[0x2:0x4])]]
+
+            #iterate over the rest of the pairs of bytes
+            entry_count = (len(entry) - 4)//2
+
+            max_poke_count = max(max_poke_count, entry_count)
+
+            for x in range(entry_count):
+                index = from_little_bytes_int(entry[0x4 + 2*x: 0x6 + 2*x])
+                trainer_temp.append(str(x) + str(temp[index]))
+
+            trainer_output.append(trainer_temp)
+
+
+
+            
+        #write header row
+        writer_head.writerow(['Index', 
+                              'Trainer Name', 
+                              'Trainer Class', 
+                              'Mystery Value', 
+                              *(f'Pokemon {x + 1}' for x in range(max_poke_count))
+                              ])
+
+        for line in trainer_output:
+            writer_head.writerow(line)
+
+
+def export_from_GARC(working_data, target_choice):
+
+    #each entry in the array is a file in the deconstructed GARC
+    working_data.trainer_binary = load_GARC(working_data.game, askopenfilename(title=f'Select Trainer Data {working_data.trainer_file_name}', defaultextension='',filetypes= [('','')]), target_choice)
+
+    working_data.pokemon_binary = load_GARC(working_data.game, askopenfilename(title=f'Select Pokemon Data {working_data.pokemon_file_name}', defaultextension='',filetypes= [('','')]), target_choice)
+
+    working_data.personal_binary = load_GARC(working_data.game, askopenfilename(title=f'Select Personal Data {working_data.pokemon_file_name}', defaultextension='',filetypes= [('','')]), 'Personal')
+
+    #build table per trainer
+    if(target_choice == 'Regular Trainers'):
+        print_regular_trainer_csv(working_data)
+    #built two seperate files, one with trainers and what Pokemon indices and the other with the Pokemon
+    else:
+        print_facility_trainer_csv(working_data)
+
+    return(working_data)
+
+
+
+
+
+def main():
+
+    #initialize variables
+    working_data = trainerdata()
+
+    #get generation
+    while True:
+        temp = input('Enter Generation, (XY, ORAS, SM, USUM)\n').upper()
+        if(temp in {'XY', 'ORAS', 'SM', 'USUM'}):
+            working_data.game = temp
+            break
+        else:
+            print(temp, 'is not valid\n\n')
+
+
+
+
+    #get text data
+    with open(os.path.join(os.getcwd(), 'paths.cfg'), 'r') as cfg:
+        temp = [line.rstrip() for line in cfg]
+
+        
+        load_text_from_csv(os.path.join(temp[0], working_data.trainer_name_list_royale[0]), working_data.trainer_name_list_royale)
+        load_text_from_csv(os.path.join(temp[0], working_data.trainer_name_list_tree[0]), working_data.trainer_name_list_tree)
+        load_text_from_csv(os.path.join(temp[0], working_data.trainer_name_list_world[0]), working_data.trainer_name_list_world)
+        load_text_from_csv(os.path.join(temp[0], working_data.ability_name_list[0]), working_data.ability_name_list)
+        load_text_from_csv(os.path.join(temp[0], working_data.item_name_list[0]), working_data.item_name_list)
+        load_text_from_csv(os.path.join(temp[0], working_data.pokemon_name_list[0]), working_data.pokemon_name_list)
+        load_text_from_csv(os.path.join(temp[0], working_data.move_name_list[0]), working_data.move_name_list)
+
+    while True:
+
+        #choose extract or rebuild
+        while True:
+            action_choice = input('Extract or rebuild GARC, or quit? (e/r/q)\n').lower()
+            if(temp in {'e', 'r', 'q'}):
+                break
+            else:
+                print(action_choice, 'is not valid\\nn')
+
+        while action_choice != 'q':
+            target_choice = input('Regular Trainers, Battle Tree, or Battle Royale? (r/t/e)\n').lower()
+            if(target_choice in {'r', 't', 'e', 'q'}):
+                break
+            else:
+                print(target_choice, 'is not valid\\nn')
+
+        target_name = 'Battle Tree' if target_choice == 't' else 'Battle Royale' if target_choice == 'e' else 'Regular Trainers'
+
+        
+
+
+        #set up the file target for display
+        match working_data.game:
+            case 'USUM':
+                match target_choice:
+                    case 'Battle Tree':
+                        working_data.pokemon_file_name = 'a/1/0/7'
+                        working_data.trainer_file_name = 'a/1/0/6'
+                    case 'Battle Royale':
+                        working_data.pokemon_file_name = 'a/2/8/3'
+                        working_data.trainer_file_name = 'a/2/8/4'
+                    case 'Regular Trainers':
+                        working_data.pokemon_file_name = 'a/2/8/1'
+                        working_data.trainer_file_name = 'a/2/8/2'
+
+
+        match action_choice:
+            case 'e':
+                export_from_GARC(working_data, target_name)
+            case 'r':
+                import_to_GARC(working_data, target_name)
+            case 'q':
+                return
+
+main()
---
+++ generic_garc_handling.py
@@ -0,0 +1,340 @@
+import csv
+from tkinter.filedialog import askdirectory, asksaveasfilename, askopenfilename
+from functools import reduce
+
+
+def load_text_from_csv(path, thing_to_do_things_to):
+    with open(path, newline = '', encoding='utf-8-sig') as csvfile:
+        reader_head = csv.reader(csvfile, dialect='excel', delimiter=',')
+        
+        #load csv into the array      
+        thing_to_do_things_to = list(reader_head)
+
+
+#read input bytestring as little-endian, return integer
+def from_little_bytes_int(byte_input, length = 0, start = 0):
+    temp = 0
+    temp_bytes = []
+    if(length == 0):
+        temp_bytes = byte_input
+    else:
+        temp_bytes = byte_input[start:start + length]
+
+
+    for x, byte in enumerate(temp_bytes):
+        temp += byte << (x*8)
+    return(temp)
+
+
+def binary_file_to_array(file_path):
+
+    with open(file_path, "r+b") as f:
+        f.seek(0, os.SEEK_END)
+        file_end = f.tell()
+        f.seek(0, 0)
+        return(list(f.read(file_end)))
+
+def deconstruct_GARC(path, game):
+    
+    bindata = binary_file_to_array(path)
+
+    #header:
+    # 0x4 Header length (4 bytes)
+    # 0x10 Data start (4 bytes)
+    # 0x14 total file length (4 bytes)
+
+    #then depends on version
+        
+    # V4
+    # 0x18 largest file size (unpadded)
+
+    # V6
+
+    # 0x18 largest file size (with padding if it exists)
+    # 0x1C largest file size (without padding, virtually always equal to the above for our purposes)
+    # 0x20 Padding value (usually 0x4)
+
+    #counting from end of whatever version you're in (so 0x4 = 0x1C in v4, 0x24 in v6)
+
+    # 0x8 FAT0 header length (counting from 0x4)
+    # 0xC, number of files (2 bytes)
+    # from 0x10, 4 bytes per file, each one is 0x10 times file number (start from 0)
+        
+
+    #from end of above, 0x4 - header length
+    # 0x8 - file count, then 
+    # then for each file, 0x01 00 00 00, then offset start, offset end, and file length, offset counting first byte of first file as 0x0
+
+    #finally, last magic word, then header length (0xC), then length of actual data (same as final offset end from previous section
+
+    #get Fat0 offset
+    FAT0_offset = 0
+    if(game in {"XY", "ORAS"}):
+        FAT0_offset = 0x1C
+    else:
+        FAT0_offset = 0x24
+        
+        
+    FATB_offset = FAT0_offset + from_little_bytes_int(bindata[FAT0_offset + 0x4:FAT0_offset + 0x8])
+
+    file_count = from_little_bytes_int(bindata[FAT0_offset + 0x8:FAT0_offset + 0xA])
+
+    data_absolute_offset = from_little_bytes_int(bindata[0x10:0x14])
+
+
+    output_array = []
+
+    #0xC is start of the actual file location/length data.
+    FATB_offset += 0xC
+
+    #iterate over the files, pulling the length from the FATB data, each file gets its own array in temp
+    for _ in range(file_count):
+            
+        #move data pointer to start of next file
+        data_offset = data_absolute_offset + from_little_bytes_int(bindata[FATB_offset + 0x4:FATB_offset + 0x8])
+        #print(data_offset)
+        #get length of current file
+        file_length = from_little_bytes_int(bindata[FATB_offset + 0xC:FATB_offset + 0x10])
+
+        #append the file to a new entry in output array
+        output_array.append(bindata[data_offset:data_offset + file_length])
+            
+
+        #the offset end is different than start + length because length is padded to multiple of 4.
+
+        #move to next file in FATB data
+        FATB_offset += 0x10
+
+    return(output_array)
+
+def reconstruct_GARC(final_binary, GARC_name):
+    
+    match GARC_name:
+        case "personal":
+            #merges concatenated file for output
+            out_file = final_binary + [reduce(lambda i, j: i+j, final_binary)]
+        case "model":
+            #merges with header for output
+            #pass two array of arrays
+            out_file = [reduce(lambda i, j: i+j, final_binary)]
+        case _:
+            out_file = final_binary
+            
+            
+    file_count = len(out_file)
+
+    temp = [0x0]*0x1C
+    FAT0_offset = 0
+
+    #magic GARC
+    temp[0:4] = [0x43, 0x52, 0x41, 0x47]
+
+    #Endian
+    temp[0x08:0xA] = [0xFF, 0xFE]
+
+    #header length and Version
+    if(poke_edit_data in {"XY", "ORAS"}):
+        temp[0x4] = 0x1C
+        temp[0xB] = 0x04
+        FAT0_offset = 0x1C
+    else:
+        temp[0x4] = 0x24
+        temp[0xB] = 0x06
+        temp.extend([0]*8)
+        FAT0_offset = 0x24
+
+    #section count
+    temp[0xC] = 0x4
+
+    #FAT0 Header allocation
+    temp.extend([0]*(0xC + 4*file_count))
+    
+    #Magic FAT0
+    temp[FAT0_offset:FAT0_offset + 4] = [0x4F, 0x54, 0x41, 0x46]
+    
+    #FAT0 length
+    temp[FAT0_offset + 0x4:FAT0_offset + 0x8] = from_int_little_bytes(file_count*4 + 0xC, 0x4)
+
+    #file count
+    temp[FAT0_offset + 0x8:FAT0_offset + 0xA] = from_int_little_bytes(file_count, 0x2)
+
+    #padding
+    temp[FAT0_offset + 0xA:FAT0_offset + 0xC] = [0xFF, 0xFF]
+
+    #write FAT0 thing
+    pointer = FAT0_offset + 0xC
+    for x in range(file_count):
+        temp[pointer:pointer + 4] = from_int_little_bytes(x * 0x10, 0x4)
+        pointer += 0x4
+
+
+    #allocate BFAT, 0xC for header, then 0x10 per file
+    temp.extend([0]*(0xC + 0x10*file_count))
+    #magic BFAT
+    temp[pointer:pointer + 4] = [0x42, 0x54, 0x41, 0x46]
+
+    pointer +=4
+
+    #BFAT length
+    temp[pointer:pointer + 4] = from_int_little_bytes(file_count*0x10 + 0xC, 0x4)
+
+    pointer +=4
+
+    #BFAT file count
+    temp[pointer:pointer + 2] = temp[FAT0_offset + 0x8:FAT0_offset + 0xA]
+
+    pointer += 4
+
+    #before we write the BFAT blocks, add the FIMB header so we can write those blocks and actual files at once
+
+    #this will point at end of file
+    fimb_pointer = len(temp)
+
+    temp.extend([0]*(0xC))
+    
+    #magic FIMB
+    temp[fimb_pointer :fimb_pointer  + 4] = [0x42, 0x4D, 0x49, 0x46]
+    
+    fimb_pointer  += 4
+
+    #FIMB header length (3 high bytes are zero)
+    temp[pointer] = [0x0C]
+
+
+    #need to update this with final offset below
+    fimb_pointer  += 4
+
+
+    data_pointer = len(temp)
+
+    #update GARC header with data start
+
+    temp[0x10:0x14] = from_int_little_bytes(data_pointer, 0x4)
+
+    offset = 0
+    biggest_size = 0
+    biggest_size_padding = 0
+    for file in out_file:
+        
+        #padding
+        temp[pointer:pointer + 4] = [0x01, 0x00, 0x00, 0x00]
+
+        #offset start
+        temp[pointer + 4: pointer + 8] = from_int_little_bytes(offset, 0x4)
+
+        length = len(file)
+        
+        
+        biggest_size = max(length, biggest_size)
+        padding = (length - 4) % 4
+        biggest_size_padding = max(length + padding, biggest_size_padding)
+        offset += length + padding
+
+        #offset end. When there is padding to z bytes, those extra bytes are filled with 0xFF, are NOT counted in the length, but ARE counted in the end-address
+        temp[pointer + 8: pointer + 0xC] = from_int_little_bytes(offset, 0x4)
+
+        #length
+        temp[pointer + 0xC: pointer + 0x10] = from_int_little_bytes(length, 0x4)
+
+
+        #extend temp by length of file
+        temp.extend([0]*length)
+        #write file to location
+        temp[data_pointer: data_pointer + length] = file
+
+        if(padding != 0):
+            temp.extend([0xFF]*padding)
+
+        data_pointer += length + padding
+
+        pointer += 0x10
+
+    #write total length of files
+    temp[fimb_pointer:fimb_pointer + 4] = from_int_little_bytes(offset, 0x4)
+
+    #in GARC header, need to write file length, and largest file size (plus padded max and padding in gen 7)
+
+    #only write largest file size at FAT0_offset - 4
+    if(poke_edit_data in {"XY", "ORAS"}):
+        temp[FAT0_offset - 0x4:FAT0_offset] = from_int_little_bytes(biggest_size, 0x4)
+    
+    #starting from FAT0_offset - 0xC:
+    #max of 0x4 and max file size
+    #max file size
+    #padding (0x4)
+    else:
+        temp[FAT0_offset - 0xC:FAT0_offset - 0x8] = from_int_little_bytes(biggest_size_padding, 0x4)
+        temp[FAT0_offset - 0x8:FAT0_offset - 0x4] = from_int_little_bytes(biggest_size, 0x4)
+        temp[FAT0_offset - 0x4:FAT0_offset] = from_int_little_bytes(0x4, 0x4)
+
+    #write total length of entire GARC
+    temp[0x14:0x18] = from_int_little_bytes(len(temp), 0x4)
+
+
+    return(temp)
+
+def save_GARC(final_binary, GARC_name, override = ''):
+
+    temp = reconstruct_GARC(final_binary, GARC_name)
+
+    match GARC_name:
+        case "personal":
+            file_path = poke_edit_data.personal_path
+        case "evolution":
+            file_path = poke_edit_data.evolution_path
+        case "levelup":
+            file_path = poke_edit_data.levelup_path
+        case "model":
+            file_path = poke_edit_data.model_path
+        case _:
+            file_path = override
+
+    with open(file_path, "w+b") as f:
+        f.write(bytes(temp))
+
+
+#loads list of filenames in extracted GARC if it exists, otherwise return empty array
+def load_GARC(game, garc_path, target):
+
+    if(os.path.exists(garc_path)):
+
+        try:
+            file_array = deconstruct_GARC(binary_file_to_array(garc_path), game)
+
+            match poke_edit_data.game:
+                case "XY":
+                    poke_edit_data.max_species_index = 721
+                case "ORAS":
+                    poke_edit_data.max_species_index = 721
+                case "SM":
+                    poke_edit_data.max_species_index = 802
+                case "USUM":
+                    poke_edit_data.max_species_index = 807
+
+            match target:
+                case "Personal":
+                    poke_edit_data.personal_path = garc_path
+
+                    #delete compilation file
+                    file_array.pop()
+
+                    poke_edit_data.personal = file_array
+                case "Levelup":
+                    poke_edit_data.levelup_path = garc_path
+                    poke_edit_data.levelup = file_array
+
+                case "Evolution":
+                    poke_edit_data.evolution_path = garc_path
+                    poke_edit_data.evolution = file_array
+
+                case _:
+                    return(file_array)
+
+
+        except Exception as e:
+            print(e)
+            return(poke_edit_data)
+
+    else:
+        print("Garc folder not found, unreadable, or empty")
+    return(poke_edit_data)
